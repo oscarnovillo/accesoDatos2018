b6ffc5bc5dd3f80363edf24504afb01c81b6586a
lucia-iesquevedo <lucia.iesquevedo@gmail.com>
2017-10-22 18:40:18 +0200

    Primer commit

---
+++ src/main/java/ad17/unit2/activities/Author.java
@@ -0,0 +1,18 @@
+package ad17.unit2.activities;
+
+public class Author {
+    private String firstName;
+    private String lastName;
+    
+    public Author(String firstName, String lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+    }
+    public String getFirstName() {
+            return firstName;
+    }
+ 
+    public String getLastName() {
+        return lastName;
+}
+}
