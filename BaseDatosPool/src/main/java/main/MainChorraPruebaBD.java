36a4fec367746164be0b8cbb9b225bfd1de10301
oscar <oscar@DESKTOP-E8BOLD9>
2018-10-27 20:33:36 +0200

    Merge origin/master
    Conflicts:
    	unit2/src/main/java/ad17/unit2/examples/XStreamAliasExample.java

--- BaseDatosPool/src/main/java/main/MainChorraPruebaBD.java
+++ BaseDatosPool/src/main/java/main/MainChorraPruebaBD.java
@@ -20,29 +20,43 @@ public class MainChorraPruebaBD {
 
   public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
-    Class.forName("com.mysql.cj.jdbc.Driver");
+//    Class.forName("com.mysql.cj.jdbc.Driver");
+//    Connection con = DriverManager
+//            .getConnection("jdbc:mysql://localhost:3310/daw2_pruebas", "root", "root");
+//     int idActor =1;
+//    String selectSql = "SELECT * FROM actor";
+//
+//    PreparedStatement stmt = con.prepareStatement(selectSql);
+//    stmt.setInt(1, idActor);
+//    ResultSet resultSet = stmt.executeQuery();
+//    
+//    while (resultSet.next())
+//    {
+//      System.out.println("fila nueva");
+//      System.out.print(resultSet.getInt(1)+" ");
+//      System.out.print(resultSet.getInt(2));
+//      System.out.println();
+//    }
+//    System.out.println("ok");
+//    
+    
+     Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
     Connection con = DriverManager
-            .getConnection("jdbc:mysql://db4free.net:3306/daw2_pruebas", "oscarnovillo", "1235b1a9");
-     int idActor =1;
-    String selectSql = "SELECT * FROM actor where fk_actor = ?";
-
-    PreparedStatement stmt = con.prepareStatement(selectSql);
-    stmt.setInt(1, idActor);
+            .getConnection("jdbc:sqlserver://localhost:1434;databaseName=dam2_pruebas;user=sa;password=@dminQbdo18");
+    PreparedStatement  stmt = con.prepareStatement("SELECT * from alumnos");
+   // stmt.setInt(1, idActor);
     ResultSet resultSet = stmt.executeQuery();
     
     while (resultSet.next())
     {
       System.out.println("fila nueva");
       System.out.print(resultSet.getInt(1)+" ");
-      System.out.print(resultSet.getInt(2));
+      System.out.print(resultSet.getString(2));
       System.out.println();
     }
-    System.out.println("ok");
-    
-    
-    resultSet.close();
-    stmt.close();
-    con.close();
+//    resultSet.close();
+//    stmt.close();
+//    con.close();
   }
 
 }
