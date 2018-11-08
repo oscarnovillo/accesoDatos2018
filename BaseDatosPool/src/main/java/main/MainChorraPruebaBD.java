1a5024a285c58cd21982fe0c348af4b42cc007a4
oscar <oscar@2DAM-PROFESOR.info.quevedo.local>
2018-11-08 11:07:55 +0100

    Merge origin/master
    Conflicts:
    	ApiClient/src/main/java/quevedo/apiclient/GoogleHttpConsumerFootball.java

--- BaseDatosPool/src/main/java/main/MainChorraPruebaBD.java
+++ BaseDatosPool/src/main/java/main/MainChorraPruebaBD.java
@@ -28,110 +28,127 @@ public class MainChorraPruebaBD {
     ResultSet resultSet = null;
     Connection con = null;
     PreparedStatement stmt = null;
-    try {
-      Class.forName("com.mysql.cj.jdbc.Driver");
+//    try {
+//      Class.forName("com.mysql.cj.jdbc.Driver");
+//
+//      con = DriverManager
+//              .getConnection("jdbc:mysql://db4free.net:3306/daw2_pruebas", "oscarnovillo", "1235b1a9");
+//      int idActor = 1;
+//      String selectSql = "SELECT * FROM actor where fk_actor = ?";
+//
+//      stmt = con.prepareStatement(selectSql);
+//      stmt.setInt(1, idActor);
+//
+//      resultSet = stmt.executeQuery();
+//
+//      while (resultSet.next()) {
+//        System.out.println("fila nueva");
+//        System.out.print(resultSet.getInt(1) + " ");
+//        System.out.print(resultSet.getInt(2));
+//        System.out.println();
+//      }
+//      System.out.println("ok");
+//
+//    } catch (ClassNotFoundException | SQLException ex) {
+//      Logger.getLogger(MainChorraPruebaBD.class.getName()).log(Level.SEVERE, null, ex);
+//    } finally {
+//      db.cerrarResultSet(resultSet);
+//      db.cerrarStatement(stmt);
+//      db.cerrarConexion(con);
+//    }
 
-      con = DriverManager
-              .getConnection("jdbc:mysql://db4free.net:3306/daw2_pruebas", "oscarnovillo", "1235b1a9");
-      int idActor = 1;
-      String selectSql = "SELECT * FROM actor where fk_actor = ?";
-
-      stmt = con.prepareStatement(selectSql);
-      stmt.setInt(1, idActor);
-
-      resultSet = stmt.executeQuery();
-
-      while (resultSet.next()) {
-        System.out.println("fila nueva");
-        System.out.print(resultSet.getInt(1) + " ");
-        System.out.print(resultSet.getInt(2));
-        System.out.println();
-      }
-      System.out.println("ok");
-
-    } catch (ClassNotFoundException | SQLException ex) {
-      Logger.getLogger(MainChorraPruebaBD.class.getName()).log(Level.SEVERE, null, ex);
-    } finally {
-      db.cerrarResultSet(resultSet);
-      db.cerrarStatement(stmt);
-      db.cerrarConexion(con);
-    }
-
-   
-    
-    try {
-      con = DriverManager
-              .getConnection("jdbc:mysql://db4free.net:3306/daw2_pruebas", "oscarnovillo", "1235b1a9");
-      int idActor = 1;
-      String selectSql = 
-              "update actor set n_oscars =? "
-              + "where fk_actor = ?";
-
-      stmt = con.prepareStatement(selectSql);
-      stmt.setInt(2, idActor);
-      stmt.setInt(1, 23);
-
-      int numeroFilas = stmt.executeUpdate();
-
-      
-      System.out.println(" filas actualizadas "+numeroFilas);
-
-    } catch ( SQLException ex) {
-      Logger.getLogger(MainChorraPruebaBD.class.getName()).log(Level.SEVERE, null, ex);
-    } finally {
-      db.cerrarStatement(stmt);
-      db.cerrarConexion(con);
-    }
-    
+//    try {
+//      con = DriverManager
+//              .getConnection("jdbc:mysql://db4free.net:3306/daw2_pruebas", "oscarnovillo", "1235b1a9");
+//      int idActor = 1;
+//      String selectSql
+//              = "update actor set n_oscars =? "
+//              + "where fk_actor = ?";
+//
+//      stmt = con.prepareStatement(selectSql);
+//      stmt.setInt(2, idActor);
+//      stmt.setInt(1, 23);
+//
+//      int numeroFilas = stmt.executeUpdate();
+//
+//      System.out.println(" filas actualizadas " + numeroFilas);
+//
+//    } catch (SQLException ex) {
+//      Logger.getLogger(MainChorraPruebaBD.class.getName()).log(Level.SEVERE, null, ex);
+//    } finally {
+//      db.cerrarStatement(stmt);
+//      db.cerrarConexion(con);
+//    }
     try {
       con = DriverManager
               .getConnection("jdbc:mysql://db4free.net:3306/daw2_pruebas", "oscarnovillo", "1235b1a9");
+
+      //comienza transaccion
+      con.setAutoCommit(false);
+
       int idActor = 1;
-      String selectSql = 
-              "delete from actor "
-              + "where fk_actor = ?";
+      String selectSql
+              = "delete from actuar "
+              + "where id_Empleado = ?";
 
       stmt = con.prepareStatement(selectSql);
-   
+
       stmt.setInt(1, 1);
 
       int numeroFilas = stmt.executeUpdate();
+      System.out.println(" filas actualizadas " + numeroFilas);
 
-      
-      System.out.println(" filas actualizadas "+numeroFilas);
+      selectSql
+              = "delete from actor "
+              + "where fk_actor = ?";
+      stmt = con.prepareStatement(selectSql);
+      stmt.setInt(1, 1);
 
-    }   catch ( SQLIntegrityConstraintViolationException ex) {
+      numeroFilas = stmt.executeUpdate();
+
+      System.out.println(" filas actualizadas " + numeroFilas);
+
+      con.commit();
+
+    } catch (SQLIntegrityConstraintViolationException ex) {
       System.out.println("FK CONTRASNTNINT");
-    }
-    catch ( SQLException ex) {
+    } catch (SQLException ex) {
+      try {
+        if (con != null) {
+          con.rollback();
+        }
+      } catch (SQLException ex1) {
+        Logger.getLogger(MainChorraPruebaBD.class.getName()).log(Level.SEVERE, null, ex1);
+      }
       Logger.getLogger(MainChorraPruebaBD.class.getName()).log(Level.SEVERE, null, ex);
     } finally {
       db.cerrarStatement(stmt);
       db.cerrarConexion(con);
     }
- 
 
-  try {
+    try {
       con = DriverManager
               .getConnection("jdbc:mysql://db4free.net:3306/daw2_pruebas", "oscarnovillo", "1235b1a9");
       int idActor = 1;
-      String selectSql = 
-              "insert into empleado (nombre,apellidos,edad) "
+      String selectSql
+              = "insert into empleado (nombre,apellidos,edad) "
               + "values (?,?,?)";
 
-      stmt = con.prepareStatement(selectSql);
-   
+      stmt = con.prepareStatement(selectSql,Statement.RETURN_GENERATED_KEYS);
+
       stmt.setString(1, "juan");
       stmt.setString(2, "guti");
       stmt.setInt(3, 21);
 
       int numeroFilas = stmt.executeUpdate();
 
-      
-      System.out.println(" filas actualizadas "+numeroFilas);
+      System.out.println(" filas actualizadas " + numeroFilas);
+      ResultSet rs = stmt.getGeneratedKeys();
+      if (rs != null && rs.next()) {
+        System.out.println(rs.getLong(1));
+      }
 
-    } 
-    catch ( SQLException ex) {
+    } catch (SQLException ex) {
       Logger.getLogger(MainChorraPruebaBD.class.getName()).log(Level.SEVERE, null, ex);
     } finally {
       db.cerrarStatement(stmt);
